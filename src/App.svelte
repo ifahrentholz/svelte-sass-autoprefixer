d12c16225f7c9002c2f021b955860cb126821eea
Ingo Fahrentholz <ingof.work@gmail.com>
2019-10-28 20:42:25 +0100

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  @value colors from "./colors.css";
+  @value myVeryNiceOrange from colors;
+  h1 {
+    color: myVeryNiceOrange;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  }
+</style>
+
+<h1>Hello {name}!</h1>
